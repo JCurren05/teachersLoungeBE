da183bceaa125c616f48a8377dbbea3860dfa121
Jonathan Curren <144627851+JCurren05@users.noreply.github.com>
2024-10-16 14:22:52 -0400

    changed .env and updated Middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+/*import jwt from "jsonwebtoken";
 
 import connection from '../database.js';
 
@@ -39,4 +39,40 @@ const userAuth = async (req, res, next) => {
     next();
 };
 
-export {userAuth};
+export {userAuth};*/
+import jwt from "jsonwebtoken";
+import connection from '../database.js';
+
+const userAuth = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token || !token.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized request" });
+  }
+
+  const tokenValue = token.split(' ')[1];
+
+  try {
+    const { email } = jwt.verify(tokenValue, process.env.JWT_SECRET);
+
+    // Perform the database query to find the user by email in the users table
+    connection.query('SELECT * FROM users WHERE email = $1', [email], function (error, results) {
+      if (error) {
+        console.error(error.stack);
+        return res.status(500).json({ message: "Server error: " + error.stack });
+      }
+
+      if (results.length === 0) {
+        return res.status(401).json({ message: "No user found with this email" });
+      }
+
+      req.userEmail = email;
+      next();
+    });
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+export { userAuth };
+
